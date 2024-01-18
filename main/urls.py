7f5adb12192b763440a5bfbf8dc1fdbdfd899ca1
seungju.kim <pipiru100@gmail.com>
2024-01-18 17:28:53 +0900

    허깅페이스 코드 init

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ app_name = "main"
 urlpatterns = [
     path("", views.index, name="index"),
     path("predict/", views.predict, name="predict"),
+    path("predict_image/", views.predict_image, name="predict_image"),
+    path("image_result/", views.image_result, name="image_result"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
 from transformers import pipeline
+from diffusers import StableDiffusionPipeline
+import torch
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel, AutoTokenizer
+import requests
+import unicodedata
+from io import BytesIO
+from PIL import Image
+
 
 
 # Create your views here.
@@ -7,8 +15,10 @@ from transformers import pipeline
 
 def index(request):
     get_prediction = request.session.get('prediction', None)
-    prediction = get_prediction[0]['generated_text']
-
+    if get_prediction is not None:
+        prediction = get_prediction[0]['generated_text']
+    else:
+        prediction = None
     return render(request, "main.html", {"prediction": prediction})
 
 
@@ -24,3 +34,36 @@ def predict(request):
     request.session['prediction'] = prediction
 
     return redirect("main:index")
+
+
+def predict_image(request):
+    data = request.FILES.get("image_file", None)
+
+    if data:
+        processor = TrOCRProcessor.from_pretrained("ddobokki/ko-trocr")
+        model = VisionEncoderDecoderModel.from_pretrained("ddobokki/ko-trocr")
+        tokenizer = AutoTokenizer.from_pretrained("ddobokki/ko-trocr")
+        # url = "https://raw.githubusercontent.com/ddobokki/ocr_img_example/master/g.jpg"
+        # response = requests.get(url)
+
+        img = Image.open(BytesIO(data.read()))
+
+        pixel_values = processor(img, return_tensors="pt").pixel_values
+        generated_ids = model.generate(pixel_values, max_length=64)
+        generated_text = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        generated_text = unicodedata.normalize("NFC", generated_text)
+        # print(generated_text)
+        request.session['image'] = generated_text
+
+    else:
+        data = request.session.get('image', None)
+        return render(request, "image_result.html")
+
+
+    return redirect("main:image_result")
+
+
+def image_result(request):
+    image = request.session.get('image', None)
+
+    return render(request, "image_result.html", {"image": image})
